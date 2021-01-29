4d38ba4722c80a583de4d242afd900f38eb12dec
youugotssponged <jordanmccann64@outlook.com>
2021-01-29 18:43:38 +0000

    .

--- Assets/Scripts/Default.cs
+++ Assets/Scripts/Default.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Default : MonoBehaviour
 {
-    public Transform
+    public Transform t;
     
     // Start is called before the first frame update
     void Start()

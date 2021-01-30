496fd52f97a4285ea950f013a454307f39bb0d58
youugotssponged <jordanmccann64@outlook.com>
2021-01-30 01:16:36 +0000

    added pause menu prefab

--- Assets/Scripts/JSON/LeaderboardModel.cs
+++
@@ -1,7 +0,0 @@
-using System;
-using UnityEngine;
-
-[System.Serializable]
-public class LeaderboardModel {
-    
-}
--- Assets/Scripts/UI/PauseMenu/PauseMenuController.cs
+++ Assets/Scripts/UI/PauseMenu/PauseMenuController.cs
@@ -1,18 +1,47 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenuController : MonoBehaviour
 {
     // Start is called before the first frame update
+    public GameObject PausePanel;
+    public bool isPaused;
+
     void Start()
     {
-        
+        isPaused = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Escape)) 
+        {
+            Debug.Log("SPACE PRESSED");
+            if(isPaused){
+                Resume();
+            } else {
+                Pause();
+            }
+        }
+    }
+
+    public void Pause(){
+        isPaused = true;
+        Time.timeScale = 0f;
+        PausePanel.SetActive(true);
+    }
+
+    public void Resume(){
+        isPaused = false;
+        Time.timeScale = 1f;
+        PausePanel.SetActive(false);
+    }
+
+    public void ReturnToMainMenu(){
+        SceneManager.LoadScene(1);
+    }
+    public void QuitToDesktop(){
+        Application.Quit();
     }
 }

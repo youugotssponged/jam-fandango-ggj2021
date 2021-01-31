d05ecaa3b44a1807618209b2a0e270d8728c1625
youugotssponged <jordanmccann64@outlook.com>
2021-01-31 00:03:50 +0000

    .

---
+++ Assets/Scripts/UI/Credits/CreditsController.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(AudioSource))]
+public class CreditsController : MonoBehaviour
+{
+    private AudioSource src;
+    public AudioClip click;
+
+    public GameObject textArea;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        src = GetComponent<AudioSource>();
+        src.clip = click;
+        src.volume = 0.7f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        textArea.transform.Translate(0, 70.0f * Time.deltaTime, 0);
+    }
+
+    public void PlayClickOnHover() 
+    {
+        src.PlayOneShot(src.clip);
+    }
+
+    public void ReturnToMainMenu(){
+        SceneManager.LoadScene(1);
+    }
+}
